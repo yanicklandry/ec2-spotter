991e6cdee0cd2af1942a1f7292579e0e675239da
slav <murphybgbs@gmail.com>
2017-03-16 11:28:29 +0200

    config_from_instance now also accepts instance id

--- fast_ai/config_from_instance.sh
+++ fast_ai/config_from_instance.sh
@@ -6,8 +6,33 @@ export maxPricePerHour=0.5
 # Set current dir to working dir - http://stackoverflow.com/a/10348989/277871
 cd "$(dirname ${BASH_SOURCE[0]})"
 
+# By default it's empty
+instance_id=
+# Read the input args
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --instance_id)
+    instance_id="$2"
+    shift # pass argument
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # pass argument or value
+done
+
 # Find the instance id by the instance name (if there are two instances with same name, use the first one)
-export instanceId=`aws ec2 describe-instances --filters Name=tag:Name,Values=$name-gpu-machine --output text --query 'Reservations[*].Instances[0].InstanceId'`
+if [ "x$instance_id" = "x" ]
+then
+	# Get the instance by name.
+	export instanceId=`aws ec2 describe-instances --filters Name=tag:Name,Values=$name-gpu-machine --output text --query 'Reservations[*].Instances[0].InstanceId'`
+else
+	# We have passed an instance id
+	instanceId=$instance_id
+fi
 
 # By default, AWS will delete this volume if the instance is terminated. 
 # We need this volume for the spot instance, so let's fix this.
